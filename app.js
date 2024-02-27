30f1d4c480060a5ee0e54bcc005c447bca6bd2ee
Leo Staf <lest0823@storsjogymnasiet.se>
2024-02-27 08:45:30 +0100

    asdsda

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const port = 3000;
 const request = require("request");
 
 app.use(express.static("public"));
+app.set("view engine", "pug");
+
 
 const apiKey = "5791ba23c43367f77aced40f3cd406c1";
 let city = "Ostersund";
@@ -16,6 +18,22 @@ let weather
 let temperatureList = []
 let timeList = []
 
+let counts = {};
+
+let day1 = [];
+let day2 = [];
+let day3 = [];
+let day4 = [];  
+let day5 = [];
+let day6 = [];
+
+let day1Time = [];
+let day2Time = [];
+let day3Time = [];
+let day4Time = [];  
+let day5Time = [];
+let day6Time = [];
+
 request(url, function (err, response, body) {
   if (err) {
     console.log("error:", err);
@@ -24,19 +42,57 @@ request(url, function (err, response, body) {
     let weather = JSON.parse(body);
     //console.log(weather["list"][0]["main"]["temp"]);
 
+    let days = [];
+
     for (i = 0; i < weather["list"].length; i++) {
       let temporary = [];
       temporary.push(Math.round((weather["list"][i]["main"]["temp"] - 273.15) * 10) / 10);
       temporary.push(weather["list"][i]["dt_txt"]);
       temperatureList.push(temporary);
+
+      if (weather["list"][i]["dt_txt"].slice(8,10) < 10) {
+        days.push(weather["list"][i]["dt_txt"].slice(9,10));
+      }
+      else {
+        days.push(weather["list"][i]["dt_txt"].slice(8,10));
+      }
+    }
+
+    days.forEach(function (x) { 
+      counts[x] = (counts[x] || 0) + 1;
+    });
+    console.log(counts);
+
+    for (i = 0; i < counts[Object.keys(counts)[0]]; i++) {
+      day1.push(Math.round((weather["list"][i]["main"]["temp"] - 273.15) * 10) / 10);
+      day1Time.push(weather["list"][i]["dt_txt"]);
+    }
+    for (i = counts[Object.keys(counts)[0]]; i < counts[Object.keys(counts)[1]] + counts[Object.keys(counts)[0]]; i++) {
+      day2.push(Math.round((weather["list"][i]["main"]["temp"] - 273.15) * 10) / 10);
+      day2Time.push(weather["list"][i]["dt_txt"]);
+    }
+    for (i = counts[Object.keys(counts)[1]] + counts[Object.keys(counts)[0]]; i < counts[Object.keys(counts)[2]] + counts[Object.keys(counts)[1]] + counts[Object.keys(counts)[0]]; i++) {
+      day3.push(Math.round((weather["list"][i]["main"]["temp"] - 273.15) * 10) / 10);
+      day3Time.push(weather["list"][i]["dt_txt"]);
+    }
+    for (i = counts[Object.keys(counts)[2]] + counts[Object.keys(counts)[1]] + counts[Object.keys(counts)[0]]; i < counts[Object.keys(counts)[3]] + counts[Object.keys(counts)[2]] + counts[Object.keys(counts)[1]] + counts[Object.keys(counts)[0]]; i++) {
+      day4.push(Math.round((weather["list"][i]["main"]["temp"] - 273.15) * 10) / 10);
+      day4Time.push(weather["list"][i]["dt_txt"]);
+    }
+    for (i = counts[Object.keys(counts)[3]] + counts[Object.keys(counts)[2]] + counts[Object.keys(counts)[1]] + counts[Object.keys(counts)[0]]; i < counts[Object.keys(counts)[4]] + counts[Object.keys(counts)[3]] + counts[Object.keys(counts)[2]] + counts[Object.keys(counts)[1]] + counts[Object.keys(counts)[0]]; i++) {
+      day5.push(Math.round((weather["list"][i]["main"]["temp"] - 273.15) * 10) / 10);
+      day5Time.push(weather["list"][i]["dt_txt"]);
+    }
+    for (i = counts[Object.keys(counts)[4]] + counts[Object.keys(counts)[3]] + counts[Object.keys(counts)[2]] + counts[Object.keys(counts)[1]] + counts[Object.keys(counts)[0]]; i < counts[Object.keys(counts)[5]] + counts[Object.keys(counts)[4]] + counts[Object.keys(counts)[3]] + counts[Object.keys(counts)[2]] + counts[Object.keys(counts)[1]] + counts[Object.keys(counts)[0]]; i++) {
+      day6.push(Math.round((weather["list"][i]["main"]["temp"] - 273.15) * 10) / 10);
+      day6Time.push(weather["list"][i]["dt_txt"]);
     }
   }
 });
 
-app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
-  res.render("index", { "temperatureList": temperatureList});
+  res.render("index", { "day1": day1, "day2": day2, "day3": day3, "day4": day4, "day5": day5, "day6": day6, "counts": counts, "day1Time": day1Time, "day2Time": day2Time, "day3Time": day3Time, "day4Time": day4Time, "day5Time": day5Time, "day6Time": day6Time });
 });
 
 app.listen(port, () => {
--- views/script.js
+++ views/script.js
@@ -1,23 +1,50 @@
 let x = 0;
 
-document.getElementById(`${x}`).style.marginLeft = "auto";
-document.getElementById(`${x}`).style.marginRight = "auto";
-console.log("amongus")
+document.getElementById(`${x}`).style.display = "flex";
 
 document.getElementById("next").addEventListener("click", function() {
-    if (x < 39) {
+    if (x < 5) {
         x++;
-        document.getElementById(`${x - 1}`).style.marginLeft = "100%";
-        document.getElementById(`${x}`).style.marginLeft = "auto";
-        document.getElementById(`${x}`).style.marginRight = "auto";
+        document.getElementById(`${x - 1}`).style.display = "none";
+        document.getElementById(`${x}`).style.display = "flex";
     }
 });
 
 document.getElementById("previous").addEventListener("click", function() {
     if (x > 0) {
         x--;
-        document.getElementById(`${x + 1}`).style.marginLeft = "100%";
-        document.getElementById(`${x}`).style.marginLeft = "auto";
-        document.getElementById(`${x}`).style.marginRight = "auto";
+        document.getElementById(`${x + 1}`).style.display = "none";
+        document.getElementById(`${x}`).style.display = "flex";
+    }
+});
+
+document.getElementById("body").addEventListener("keydown", (event) => {
+    if (event.isComposing || event.keyCode === 39) {
+        if (x < 5) {
+            x++;
+            document.getElementById(`${x - 1}`).style.display = "none";
+            document.getElementById(`${x}`).style.display = "flex";
+        }
+    }
+    else if (event.isComposing || event.keyCode === 68) {
+        if (x < 5) {
+            x++;
+            document.getElementById(`${x - 1}`).style.display = "none";
+            document.getElementById(`${x}`).style.display = "flex";
+        }
+    }
+    else if (event.isComposing || event.keyCode === 65) {
+        if (x > 0) {
+            x--;
+            document.getElementById(`${x + 1}`).style.display = "none";
+            document.getElementById(`${x}`).style.display = "flex";
+        }
+    }
+    else if (event.isComposing || event.keyCode === 37) {
+        if (x > 0) {
+            x--;
+            document.getElementById(`${x + 1}`).style.display = "none";
+            document.getElementById(`${x}`).style.display = "flex";
+        }
     }
 });
